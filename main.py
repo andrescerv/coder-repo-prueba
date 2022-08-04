b8093d129dd9039134e6876126d00cb336bfcbbe
andrescerv <44825347+andrescerv@users.noreply.github.com>
2022-08-03 23:27:05 -0600

    cambié los espacios para mayor legibilidad

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd # 3er cambio
 
+
+
 print('hola mundo')
+print('estamos creando la rama DEVEL') # hago otro comment en la rama DEVEL
 # este es un hotfix que ha sido arreglado :), ahora hemos hecho los útlimos cambios
 
-print('estamos creando la rama DEVEL') # hago otro comment en la rama DEVEL
 
-
+# indentando para mayor legibilidad (2do cambio en _release1)
 df = pd.DataFrame() # 1er cambio en feature1
 
 for i in len(3): # creando el 4to cambio en devel
