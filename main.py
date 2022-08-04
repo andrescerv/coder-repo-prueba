c5fe0d70e955f610a0c860ae6554806765e18b75
andrescerv <44825347+andrescerv@users.noreply.github.com>
2022-08-03 23:10:49 -0600

    creando for loop

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import pandas as pd # 3er cambio
 print('hola mundo')
 
 print('estamos creando la rama DEVEL') # hago otro comment en la rama DEVEL
+
+for i in len(3): # creando el 4to cambio en devel
+    print(i)
